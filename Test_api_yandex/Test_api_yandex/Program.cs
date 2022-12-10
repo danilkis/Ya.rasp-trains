5d776239b1130e63c0c7c0e24528d08ffe742204
danilkis <danyapancresnicov@gmail.com>
2022-12-10 09:52:31 +0300

    update 1.3
    Теперь как библиотека. Фиксы работы с датами и прочие изменения

---
+++ Test_api_yandex/Test_api_yandex/Program.cs
@@ -0,0 +1,25 @@
+﻿using API.Yandex.Rasp;
+using Newtonsoft.Json.Linq;
+var auth = new Auth();
+auth.key = ""; //Ваш ключ тут
+DateTime data = DateTime.Today;
+string ID1 = "s9602496"; 
+string ID2 = "s9603134";
+List<Route> station_sch;
+station_sch = get.station_schedule(data, ID1,auth);
+foreach (Route station in station_sch) 
+{
+    if (station.depart != null) { Console.WriteLine("---> " + station.depart); }
+    else if (station.arrival != null)  { Console.WriteLine("<--- " + station.arrival); }
+}
+List<Route> route1 = get.route(data, ID1, ID2, auth);
+foreach (Route route in route1)
+{
+    Console.WriteLine(route.title + "  " + route.depart + "  " + route.arrival);
+}
+List<Station> stations;
+stations = get.stationID(auth);
+foreach (Station station1 in stations) 
+{
+    Console.WriteLine(station1.code + "  " + station1.title);
+}
